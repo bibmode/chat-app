2b7229eaf9878090b0ef01f688f4a016f604cfd0
bibmode <vievenavales29@gmail.com>
2022-03-19 14:18:21 +0800

    setup

---
+++ components/Message.js
@@ -0,0 +1,22 @@
+const Message = ({ name, date, message }) => {
+  return (
+    <div className="py-4 grid grid-cols-8 gap-2 max-h-fit">
+      {/* image */}
+      <div className="bg-white grid row-span-2 h-9 w-9 rounded-md place-content-center">
+        {name[0].toUpperCase()}
+      </div>
+
+      {/* details */}
+      <div className="pl-2 flex col-start-2 col-end-9 row-span-1">
+        <h3 className="text-gray-400 pr-3 font-semibold">{name}</h3>
+        <h4 className="text-gray-500 text-sm">{date}</h4>
+      </div>
+
+      <p className="pl-2 text-gray-50 col-start-2 col-end-9 row-end-auto">
+        {message}
+      </p>
+    </div>
+  );
+};
+
+export default Message;
---
+++ data/messages.js
@@ -0,0 +1,46 @@
+export const messages = {
+  data: [
+    {
+      name: "Nellie Francis",
+      date: "yesterday at 2:29 AM",
+      message:
+        "Suspendisse enim tellus, elementum quis dictum sed, sodales at mauris 😀",
+    },
+    {
+      name: "Annaliese Huynh",
+      date: "yesterday at 2:29 AM",
+      message:
+        "Orci varius natoque penatibus et magnis dis parturient montes 😀",
+    },
+    {
+      name: "Xanthe Neal",
+      date: "yesterday at 1:29 PM",
+      message:
+        "Etiam eleifend fermentum ipsum eu rhoncus. In non justo aliquam, imperdiet metus id, tincidunt orci 😍",
+    },
+    {
+      name: "Denzel Barrett",
+      date: "yesterday at 2:39 PM",
+      message:
+        "Etiam eleifend fermentum ipsum eu rhoncus. In non justo aliquam, imperdiet metus id, tincidunt orci 😍",
+    },
+    {
+      name: "Nellie Francis",
+      date: "today at 2:29 AM",
+      message:
+        "Class aptent taciti sociosqu ad litora torquent per conubia nostra 😀",
+    },
+    {
+      name: "Shaunna Firth",
+      date: "today at 1:29 PM",
+      message:
+        "Morbi eget turpis ut massa luctus cursus. Sed sit amet risus quis neque condimentum aliquet. Phasellus consequat et justo eu accumsan 🙌. Proin pretium id nunc eu molestie. Nam consectetur, ligula vel mattis facilisis, ex mauris venenatis nulla, eget tempor enim neque eget massa 🤣",
+    },
+    {
+      name: "Denzel Barrett",
+      date: "today at 2:39 PM",
+      message:
+        "Aenean tempus nibh vel est lobortis euismod. Vivamus laoreet viverra nunc 🐶",
+    },
+  ],
+};
